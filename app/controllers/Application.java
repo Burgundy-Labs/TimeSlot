c4401fde552ccf9910b6fd5304a041e6a3d4adcf
Caden Sumner <cadenjsumner@gmail.com>
2017-10-15 14:47:57 -0400

    maybe fixed layout? (??) ???

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,9 +6,9 @@ import java.util.List;
 
 public class Application {
     public static List<MenuLink> menuLinks = Arrays.asList(
-            new MenuLink("/Account", "Account", "account_circle", "Manage account settings / preferences."),
+            new MenuLink("/Account", "Account", "account_circle", "Manage account settings and preferences."),
             new MenuLink("/Dashboard", "Dashboard", "dashboard", "Something about the dashboard."),
-            new MenuLink("/Appointments", "Appointments", "schedule", "View, manage, or cancel your upcoming / past appointments."),
+            new MenuLink("/Appointments", "Appointments", "schedule", "View / manage your upcoming and past appointments."),
             new MenuLink("/Coaches", "Coaches", "recent_actors", "View all coaches working at the center."),
             new MenuLink("/Center", "Center", "school", "View all coaches' appointments at the center.")
     );
