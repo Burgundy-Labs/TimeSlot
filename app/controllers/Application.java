59d23f179c858b6656440e87fa1511a6aa501667
Caden Sumner <cadenjsumner@gmail.com>
2017-10-15 11:32:17 -0400

    Fix sidebar hover, cleanup code, modularize menu links.
    See commit title

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,21 +1,19 @@
 package controllers;
 
-import play.mvc.*;
+import controllers.ApplicationComponents.MenuLink;
+import java.util.Arrays;
+import java.util.List;
 
-/**
- * This controller contains an action to handle HTTP requests
- * to the application's home page.
- */
-public class Application extends Controller {
-
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
-    public Result index() {
-        return ok(views.html.index.render("Login to Project Burgundy"));
-    }
-
+public class Application {
+    public static List<MenuLink> menuLinks = Arrays.asList(
+            new MenuLink("/Account", "Account", "account_circle", "Manage account settings / preferences."),
+            new MenuLink("/Dashboard", "Dashboard", "dashboard", "Something about the dashboard."),
+            new MenuLink("/Appointments", "Appointments", "schedule", "View, manage, or cancel your upcoming / past appointments."),
+            new MenuLink("/Coaches", "Coaches", "recent_actors", "View all coaches working at the center."),
+            new MenuLink("/Center", "Center", "school", "View all coaches' appointments at the center.")
+    );
 }
+
+
+
+
