2ed4fbcf356f1b815394e5c2fca09d3e484e5551
Caden Sumner <cadenjsumner@gmail.com>
2017-10-31 15:25:23 -0400

    Updated model info + add users on login

--- app/controllers/CoachesController.java
+++ app/controllers/CoachesController.java
@@ -11,16 +11,7 @@ import java.util.Map;
 
 public class CoachesController extends Controller {
     public Result index() {
-        UsersModel testUser = new UsersModel();
-        testUser.setEmail("test" + Math.random()+ "@test.com");
-        testUser.setUserId((""+ (int)(Math.random() * 10)));
-        testUser.setUsername("TESTER TEST");
-        Map<String,Boolean> roles = new HashMap<>();
-        roles.put("admin", true);
-        roles.put("coach", true);
-        testUser.setRoles(roles);
 
-        new UserDB().addUser(testUser);
         return ok(views.html.coaches.render());
     }
 }
--- app/controllers/LoginController.java
+++ app/controllers/LoginController.java
@@ -1,30 +1,34 @@
 package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import models.UsersModel;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
-
-import java.util.Map;
 
 public class LoginController extends Controller {
     public Result index() {
         return ok(views.html.login.render());
     }
-
     /*
     *  Retrieves information from currently signed in user (just signed in)
     *  Used to identify if they are new, and inputting them into the DB if so.
     */
     public Result signedIn() {
         /* Get user object from request */
-        JsonNode user = request().body().asJson();
-
+        JsonNode json = request().body().asJson();
+        /* Get user from json request */
+        UsersModel user = Json.fromJson(json, UsersModel.class);
         /* Store UID in Session */
-        session("signedInUser", user.get("uid").asText());
-
+        session("signedInUser", user.getUid());
         /* Check if user is in DB */
-
+        new UserDB();
+        if(UserDB.getUser(user.getUid()) != null) {/* Don't upload existing users */}
+        else {
+            /* Add user with default role of student  */
+            user.setRole("student");
+            new UserDB().addUser(user);
+        }
         /* Add user to DB with 'student' role (default) */
         return ok();
     }
