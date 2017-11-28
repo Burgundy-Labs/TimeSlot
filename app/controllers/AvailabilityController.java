b3d6faf1b517fd3db6fe2104f0fd843857280e14
Caden Sumner <cadenjsumner@gmail.com>
2017-11-28 10:50:59 -0500

    Building availability for coaches

---
+++ app/controllers/AvailabilityController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class AvailabilityController extends Controller {
+
+    public Result createAvailability() {
+
+        return ok();
+    }
+}
