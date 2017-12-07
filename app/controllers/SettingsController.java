25719577f7b273d8f669f06e6a3ba5ec96fa5e2d
Caden Sumner <cadenjsumner@gmail.com>
2017-12-06 20:46:58 -0500

    Settings

--- app/controllers/SettingsController.java
+++ app/controllers/SettingsController.java
@@ -2,13 +2,14 @@ package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import controllers.Databases.SettingsDB;
-import controllers.Databases.UserDB;
 import models.AppointmentTypeModel;
+import models.ServiceModel;
 import models.SettingsModel;
-import models.UsersModel;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
+
+import java.util.List;
 
 public class SettingsController extends Controller {
 
@@ -27,13 +28,41 @@ public class SettingsController extends Controller {
     }
 
     public Result createAppointmentType() {
-        /* Get user object from request */
         JsonNode json = request().body().asJson();
-        /* Get user from json request */
         String appointmentTypeName = json.findPath("appointmentTypeName").asText();
         AppointmentTypeModel appointmentType = new AppointmentTypeModel(null, appointmentTypeName);
-        /* Check if user is in DB */
+        SettingsDB.addAppointmentType(appointmentType);
         return ok();
+    }
+
+    public static List<AppointmentTypeModel> getAppointmentTypes() {
+        return SettingsDB.getAppointmentTypes();
+    }
+
+    public Result createService() {
+        JsonNode json = request().body().asJson();
+        String serviceName = json.findPath("serviceName").asText();
+        ServiceModel service = new ServiceModel(null, serviceName);
+        SettingsDB.addService(service);
+        return ok();
+    }
+
+    public Result removeAppointmentType() {
+        JsonNode json = request().body().asJson();
+        String appointmentTypeId = json.findPath("appointmentTypeId").asText();
+        SettingsDB.removeAppointmentType(appointmentTypeId);
+        return ok();
+    }
+
+    public Result removeService() {
+        JsonNode json = request().body().asJson();
+        String serviceId = json.findPath("serviceId").asText();
+        SettingsDB.removeService(serviceId);
+        return ok();
+    }
+
+    public static List<ServiceModel> getServices() {
+        return SettingsDB.getServices();
     }
 
     public static SettingsModel getSettings() {
