cc974d426a27e521ee86f39ecce6e775e706e630
Caden Sumner <cadenjsumner@gmail.com>
2017-12-07 14:33:47 -0500

    Feedback Page Finished

--- app/controllers/FeedbackController.java
+++ app/controllers/FeedbackController.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import controllers.Databases.SettingsDB;
 import play.mvc.Controller;
 import play.mvc.Result;
 
@@ -8,5 +10,31 @@ public class FeedbackController extends Controller {
         return ok(views.html.feedback.render());
     }
 
+    public Result sendFeedback() {
+        JsonNode json = request().body().asJson();
 
+        String name = json.findPath("name").asText();
+        String email = json.findPath("email").asText();
+        String feedback = json.findPath("feedback").asText();
+        MailerService.sendEmail(
+                "New Feedback From " + name,
+                name,
+                email,
+                "Project Burgundy",
+                "contact.project.burgundy@gmail.com",
+                "<p style='font-size:48px;color:#17C671;text-align:center;'>&#9997;</p><h1 style='text-align:center;'>New feedback from " + name + "!</h1> " +
+                        "<h3>Name:</h3>" +
+                        "<div style=\"background-color:#EFF0F1; border-radius:4px; padding: 10px;\">"
+                        + name +
+                        "</div></br>" +
+                        "<h3>Email:</h3>" +
+                        "<div style=\"background-color:#EFF0F1; border-radius:4px; padding: 10px;\">"
+                        + email +
+                        "</div></br>" +
+                        "<h3>Feedback:</h3>" +
+                        "<div style=\"background-color:#EFF0F1; border-radius:4px; padding: 10px;\">"
+                        + feedback +
+                        "</div></br>");
+        return ok();
+    }
 }
