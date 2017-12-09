dee8d6013de3365969b5522f522cf5d28a832aa5
Caden Sumner <cadenjsumner@gmail.com>
2017-12-09 16:55:18 -0500

    Service to coach dropdown

--- app/controllers/AccountController.java
+++ app/controllers/AccountController.java
@@ -1,14 +1,35 @@
 package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import controllers.Databases.UserDB;
+import models.ServiceModel;
 import models.UsersModel;
 import play.Logger;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AccountController extends Controller {
     public Result index() {
         return ok(views.html.account.render());
+    }
+
+    public static List<ServiceModel> getAvailableServices(String userId) {
+
+        List<ServiceModel> availableServices = SettingsController.getServices();
+        List<ServiceModel> coachServices = UserDB.getServicesForUser(userId);
+
+        for(ServiceModel s : availableServices) {
+            for(ServiceModel c : coachServices) {
+                if(s.getServiceId().equals(c.getServiceId())){
+                    availableServices.remove(s);
+                }
+            }
+        }
+
+        return availableServices;
     }
 }
