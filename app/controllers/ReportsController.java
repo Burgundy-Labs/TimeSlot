3ec07eab084f53c76141b37d8bf2b627ed781a01
Caden Sumner <cadenjsumner@gmail.com>
2017-12-14 16:23:53 -0500

    Minor color changes + report page

--- app/controllers/ReportsController.java
+++ app/controllers/ReportsController.java
@@ -1,11 +1,24 @@
 package controllers;
 
+import controllers.Databases.AppointmentsDB;
+import models.AppointmentsModel;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
+
+import java.util.List;
 
 public class ReportsController extends Controller {
 
     public Result index() {
         return ok(views.html.reports.render());
+    }
+
+    public Result appointmentTypeStatistics() {
+        List<AppointmentsModel> appointments = AppointmentsDB.getAppointments();
+
+
+
+        return ok(Json.toJson(appointments));
     }
 }
