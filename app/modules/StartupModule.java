d634ea872383fa1926b303c97017b1d0ac0629bc
Caden Sumner <cadenjsumner@gmail.com>
2017-12-14 10:51:02 -0500

    Add Reports + protect menu links + begin fixing appointment any coach

--- app/modules/StartupModule.java
+++ app/modules/StartupModule.java
@@ -17,28 +17,9 @@ public class StartupModule extends AbstractModule {
     protected void configure() {
         /* Start the database service */
         FirestoreDB.initialize();
-        /* Define menu links */
-        new MenuLinks("/Account", "Account", "account_circle", "Manage your account settings and preferences.");
-        new MenuLinks("/Dashboard", "Dashboard", "dashboard", "Something about the dashboard.");
-        new MenuLinks("/Appointments", "Appointments", "schedule", "View / manage your upcoming and past appointments.");
-        new MenuLinks("/Coaches", "Coaches", "recent_actors", "View all coaches working at the center.");
-        new MenuLinks("/Center", "Center", "school", "View all coaches' appointments at the center.");
-        new MenuLinks("/Settings","Site Settings", "settings","Manage settings related to the center");
-
         /* Define default roles for project */
         new Roles("Coach");
         new Roles("Admin");
         new Roles("Student");
-
-        /* Define default appointment types */
-        new AppointmentTypes("One-Time", "One-on-one with a coach in the center.");
-        new AppointmentTypes("Weekly" ,"A recurring weekly regular appointment.");
-        new AppointmentTypes("Online", "Submit a paper for a coach to read and receive comments back.");
-
-        /* Define default services */
-        new Services("Writing");
-        new Services("Speaking");
-        new Services("Studying");
-
     }
 }
