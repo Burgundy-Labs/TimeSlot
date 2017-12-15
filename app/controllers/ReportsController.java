d2fca24af3663a8952ed88fa3fad396fcddf78b6
Caden Sumner <cadenjsumner@gmail.com>
2017-12-14 21:01:54 -0500

    Appointment Type + Services Charts

--- app/controllers/ReportsController.java
+++ app/controllers/ReportsController.java
@@ -4,6 +4,7 @@ import controllers.Databases.AppointmentsDB;
 import controllers.Databases.SettingsDB;
 import models.AppointmentTypeModel;
 import models.AppointmentsModel;
+import models.ServiceModel;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -29,5 +30,16 @@ public class ReportsController extends Controller {
             appointmentTypeCounts.put(a.getAppointmentType(), appointmentTypeCounts.get(a.getAppointmentType()) + 1);
         }
         return ok(Json.toJson(appointmentTypeCounts));
+    }
+
+    public Result serviceStatistics() {
+        List<AppointmentsModel> appointments = AppointmentsDB.getAppointments();
+        List<ServiceModel> services = SettingsDB.getServices();
+        Map<String, Integer> serviceCounts = services.stream().collect(Collectors.toMap(ServiceModel::getService, i -> 0));
+
+        for (AppointmentsModel a: appointments ) {
+            serviceCounts.put(a.getServiceType(), serviceCounts.get(a.getServiceType()) + 1);
+        }
+        return ok(Json.toJson(serviceCounts));
     }
 }
