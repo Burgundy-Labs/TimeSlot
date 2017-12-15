72c5e673f1af55959a46d4ca2a072f89b37da0a8
Caden Sumner <cadenjsumner@gmail.com>
2017-12-14 20:53:57 -0500

    Charts work

--- app/controllers/ReportsController.java
+++ app/controllers/ReportsController.java
@@ -1,12 +1,18 @@
 package controllers;
 
 import controllers.Databases.AppointmentsDB;
+import controllers.Databases.SettingsDB;
+import models.AppointmentTypeModel;
 import models.AppointmentsModel;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ReportsController extends Controller {
 
@@ -16,9 +22,12 @@ public class ReportsController extends Controller {
 
     public Result appointmentTypeStatistics() {
         List<AppointmentsModel> appointments = AppointmentsDB.getAppointments();
+        List<AppointmentTypeModel> appointmentTypes = SettingsDB.getAppointmentTypes();
+        Map<String, Integer> appointmentTypeCounts = appointmentTypes.stream().collect(Collectors.toMap(AppointmentTypeModel::getAppointmentType, i -> 0));
 
-
-
-        return ok(Json.toJson(appointments));
+        for (AppointmentsModel a: appointments ) {
+            appointmentTypeCounts.put(a.getAppointmentType(), appointmentTypeCounts.get(a.getAppointmentType()) + 1);
+        }
+        return ok(Json.toJson(appointmentTypeCounts));
     }
 }
