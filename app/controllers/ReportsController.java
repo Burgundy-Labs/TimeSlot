142eb2afabfaabfd73d723a2af9e30a488ff1e6d
Caden Sumner <cadenjsumner@gmail.com>
2017-12-14 22:23:30 -0500

    Test

--- app/controllers/ReportsController.java
+++ app/controllers/ReportsController.java
@@ -5,23 +5,28 @@ import controllers.Databases.SettingsDB;
 import models.AppointmentTypeModel;
 import models.AppointmentsModel;
 import models.ServiceModel;
+import models.UsersModel;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class ReportsController extends Controller {
 
     public Result index() {
-        return ok(views.html.reports.render());
+        UsersModel currentUser = UserController.getCurrentUser();
+        if( currentUser ==  null || !currentUser.getRole().equals("Admin")){
+            return ok(views.html.dashboard.render());
+        } else {
+            return ok(views.html.reports.render());
+        }
     }
 
-    public Result appointmentTypeStatistics() {
+    public Result appointmentTypeStatistics(Long reportStart, Long reportEnd) {
+        Date start = new Date(reportStart);
+        Date end = new Date(reportEnd);
         List<AppointmentsModel> appointments = AppointmentsDB.getAppointments();
         List<AppointmentTypeModel> appointmentTypes = SettingsDB.getAppointmentTypes();
         Map<String, Integer> appointmentTypeCounts = appointmentTypes.stream().collect(Collectors.toMap(AppointmentTypeModel::getAppointmentType, i -> 0));
@@ -32,8 +37,10 @@ public class ReportsController extends Controller {
         return ok(Json.toJson(appointmentTypeCounts));
     }
 
-    public Result serviceStatistics() {
-        List<AppointmentsModel> appointments = AppointmentsDB.getAppointments();
+    public Result serviceStatistics(Long reportStart, Long reportEnd) {
+        Date start = new Date(reportStart);
+        Date end = new Date(reportEnd);
+        List<AppointmentsModel> appointments = AppointmentsDB.getAppointmentsByDate(start,end);
         List<ServiceModel> services = SettingsDB.getServices();
         Map<String, Integer> serviceCounts = services.stream().collect(Collectors.toMap(ServiceModel::getService, i -> 0));
 
