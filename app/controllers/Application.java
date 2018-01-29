83784cd0ca7b208039fb084441e9735a032e5af2
Caden Sumner <cadenjsumner@gmail.com>
2018-01-29 01:10:00 -0500

    Migrate appointment list into JS template

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -32,7 +32,7 @@ public class Application {
             new MenuLinks("/Dashboard", "Dashboard", "dashboard", "Something about the dashboard.", false, false),
             new MenuLinks("/Appointments", "Appointments", "schedule", "View / manage your upcoming and past appointments.", false, false),
             new MenuLinks("/Coaches", "Coaches", "recent_actors", "View all users working at the center.",false, false),
-            new MenuLinks("/Center", "Center", "school", "View all users' appointments at the center.",true,true ),
+            new MenuLinks("/Center", "Center", "school", "View all users' appointments at the center.",true,false ),
             new MenuLinks("/Settings","Site Settings", "settings","Manage settings related to the center",true,false ) ,
             new MenuLinks("/Reports", "Reports", "assessment", "View reports and statistics about the center", true, false)
     );
