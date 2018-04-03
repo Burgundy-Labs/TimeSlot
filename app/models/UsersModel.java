da9e367d3c9b44f5e669caff4494815564e86a41
Caden Sumner <cadenjsumner@gmail.com>
2018-04-03 00:35:32 -0400

    This line broke the server

--- app/models/UsersModel.java
+++ app/models/UsersModel.java
@@ -86,7 +86,7 @@ public class UsersModel {
     }
 
     public Boolean isCoach() {
-        return isCoach || (getRole().equals("Coach"));
+        return isCoach || (getRole() != null && getRole().equals("Coach"));
     }
 
     public void setIsCoach(Boolean coach) {
