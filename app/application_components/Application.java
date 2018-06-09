99f46ff4037df6bfcb9def057fd4647bb9ba50ce
JacobFergus <jacobfer@mtu.edu>
2018-06-09 17:38:34 -0400

    Good Practices

--- app/application_components/Application.java
+++ app/application_components/Application.java
@@ -4,18 +4,26 @@ import com.google.inject.Inject;
 import com.typesafe.config.Config;
 import controllers.UserController;
 import play.Environment;
-import play.mvc.Result;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static play.mvc.Results.forbidden;
+
 
 /* TODO look into better use of DI to remove static methods */
 public class Application {
     private static Config config;
     private static Environment environment;
-    private UserController userController = new UserController();
+
+    public static List<MenuLinks> menuLinks = Arrays.asList(
+            new MenuLinks("/Account", "Account", "account_circle", "Manage your account settings and preferences.", false, false),
+            new MenuLinks("/Dashboard", "Dashboard", "dashboard", "Something about the dashboard.", false, false),
+            new MenuLinks("/Appointments", "Appointments", "schedule", "View / manage your upcoming and past appointments.", false, false),
+            new MenuLinks("/Coaches", "Coaches", "recent_actors", "View all users working at the center.",false, false),
+            new MenuLinks("/Center", "Center", "school", "View all users' appointments at the center.",true,false ),
+            new MenuLinks("/Settings","Site Settings", "settings","Manage settings related to the center",true,false ) ,
+            new MenuLinks("/Reports", "Reports", "assessment", "View reports and statistics about the center", true, false)
+    );
 
     @Inject
     public Application(Config config, Environment environment) {
@@ -35,15 +43,6 @@ public class Application {
         return config;
     }
 
-    public static List<MenuLinks> menuLinks = Arrays.asList(
-            new MenuLinks("/Account", "Account", "account_circle", "Manage your account settings and preferences.", false, false),
-            new MenuLinks("/Dashboard", "Dashboard", "dashboard", "Something about the dashboard.", false, false),
-            new MenuLinks("/Appointments", "Appointments", "schedule", "View / manage your upcoming and past appointments.", false, false),
-            new MenuLinks("/Coaches", "Coaches", "recent_actors", "View all users working at the center.",false, false),
-            new MenuLinks("/Center", "Center", "school", "View all users' appointments at the center.",true,false ),
-            new MenuLinks("/Settings","Site Settings", "settings","Manage settings related to the center",true,false ) ,
-            new MenuLinks("/Reports", "Reports", "assessment", "View reports and statistics about the center", true, false)
-    );
 }
 
 
