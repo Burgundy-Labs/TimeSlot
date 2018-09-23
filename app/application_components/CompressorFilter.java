41557ae30134f239044f8b6b67330a28a66e0487
Caden Sumner <cadenjsumner@gmail.com>
2018-09-23 15:18:33 -0400

    Fix compressor & application.conf
    Requires new applicaiton.conf

---
+++ app/application_components/CompressorFilter.java
@@ -0,0 +1,52 @@
+package application_components;
+
+import akka.stream.Materializer;
+import com.googlecode.htmlcompressor.compressor.HtmlCompressor;
+import com.mohiva.play.htmlcompressor.HTMLCompressorFilter;
+import play.Environment;
+import play.Mode;
+import play.api.Configuration;
+
+import javax.inject.Inject;
+
+public class CompressorFilter extends HTMLCompressorFilter {
+
+    private Configuration configuration;
+    private Environment environment;
+    private Materializer mat;
+
+    @Inject
+    public CompressorFilter(
+            Configuration configuration, Environment environment, Materializer mat) {
+
+        this.configuration = configuration;
+        this.environment = environment;
+        this.mat = mat;
+    }
+
+    @Override
+    public Configuration configuration() {
+        return configuration;
+    }
+
+    @Override
+    public HtmlCompressor compressor() {
+        HtmlCompressor compressor = new HtmlCompressor();
+        if (environment.mode() == Mode.DEV) {
+            compressor.setPreserveLineBreaks(true);
+        }
+
+        compressor.setRemoveComments(true);
+        compressor.setRemoveIntertagSpaces(true);
+        compressor.setRemoveHttpProtocol(true);
+        compressor.setRemoveHttpsProtocol(false);
+        compressor.setCompressCss(true);
+        return compressor;
+    }
+
+    @Override
+    public Materializer mat() {
+        return mat;
+    }
+
+}
--- app/databases/FirestoreHandler.java
+++ app/databases/FirestoreHandler.java
@@ -3,6 +3,8 @@ package databases;
 import application_components.Application;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreOptions;
+import com.google.cloud.firestore.SetOptions;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
@@ -13,24 +15,29 @@ import java.io.IOException;
 
 /* Sets up the initial FireStore Client for DBs to utilize */
 public class FirestoreHandler {
-
+    private static boolean setup = false;
+    private static FirestoreOptions firestoreOptions;
     public static Firestore get() {
-        File serviceAccount = Application.getEnvironment().getFile("conf/credentials.json");
-        GoogleCredentials credentials = null;
-        try {
-            credentials = GoogleCredentials.fromStream(new FileInputStream(serviceAccount));
-        } catch (IOException e) {
-            e.printStackTrace();
+
+        if(!setup) {
+            File serviceAccount = Application.getEnvironment().getFile("conf/credentials.json");
+            GoogleCredentials credentials = null;
+            try {
+                credentials = GoogleCredentials.fromStream(new FileInputStream(serviceAccount));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            assert credentials != null;
+            FirestoreOptions options = FirestoreOptions.newBuilder()
+                    .setCredentials(credentials).setTimestampsInSnapshotsEnabled(true)
+                    .build();
+            options.getService();
+            firestoreOptions = options;
+            setup = true;
         }
-        assert credentials != null;
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(credentials)
-                .build();
-        /* Ensures only one FirebaseApp is running */
-        if (!(FirebaseApp.getApps().size() > 0)) {
-            FirebaseApp.initializeApp(options);
-        }
-        return FirestoreClient.getFirestore();
+
+        return firestoreOptions.getService();
+
     }
 }
 
