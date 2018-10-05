d7879b5c959b8ffa4fc452b1ac15f2c0b57ab9d4
Kevin Brill <kevinjbrill@utexas.edu>
2018-10-04 21:35:03 -0500

    Basic fields added to GroupsModel
    Feel free to look at this and edit as needed. I'm still learning the setup of the project

--- app/models/GroupsModel.java
+++ app/models/GroupsModel.java
@@ -1,5 +1,47 @@
 package models;
 
+import java.util.List;
+
 public class GroupsModel {
-    
+    private String displayName; //Group's name
+    private String photoURL; // Google-hosted profile photo
+    private String uid; // Unique identifier provided by Google
+    private String role; // "User", "Coach", "Admin"
+    private List<String> attributes; // Used to store additional information
+    /**
+     * Not sure what data type this list should be in. Can't remember if it's
+     * good practice to use models like this
+    private List<UsersModel> usersList;
+    **/
+
+
+    public GroupsModel() { }
+
+    public GroupsModel(String displayName, String photoURL, String uid, String role, List<String> attributes) {
+        this.displayName = displayName;
+        this.photoURL = photoURL;
+        this.uid = uid;
+        this.role = role;
+        this.attributes = attributes;
+    }
+
+    public String getDisplayName() { return displayName; }
+
+    public void setDisplayName(String displayName) { this.displayName = displayName; }
+
+    public String getPhotoURL() { return photoURL; }
+
+    public void setPhotoURL(String photoURL) { this.photoURL = photoURL; }
+
+    public String getUid() { return uid; }
+
+    public void setUid(String uid) { this.uid = uid; }
+
+    public String getRole() { return role; }
+
+    public void setRole(String role) { this.role = role; }
+
+    public List<String> getAttributes() { return attributes; }
+
+    public void setAttributes(List<String> attributes) { this.attributes = attributes; }
 }
